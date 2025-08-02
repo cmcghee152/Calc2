fb75d09c2f7e649494d15ae1dbffedf2b16e15ac
cmcghee152 <cmcghee987@gmail.com>
2025-08-02 14:58:42 -0400

    Updates

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+function appendToDisplay (value) {
+    const display = document.getElementById('display');
+    display.value += value;
+}
 
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+    
+function evaluateExp() {
+    const display = document.getElementById('display');
+    try {
+        const expression = display.value
+        .replace(/x/g, '*')
+        .replace(/÷/g, '/');  
+        const result = math.evaluate(expression);
+        display.value = result;
+    } 
+    catch (err) {
+    display.value = 'Error';
+    }
+}
